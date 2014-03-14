f6219e39c871454df1326ed495a826d02b6832a7
Michael Welch <michaelgwelch@gmail.com>
2014-03-14 18:22:32 -0500

    Create instance Monad Reader and refactor nullAP

--- BACnet/src/ReaderAP.hs
+++ BACnet/src/ReaderAP.hs
@@ -1,12 +1,33 @@
 module ReaderAP (bacRead, nullAP) where
 
 import qualified Data.ByteString as BS
+import Data.Word
 
 data Reader a = R (BS.ByteString -> (Maybe a, BS.ByteString))
 
 bacRead :: Reader a -> BS.ByteString -> (Maybe a, BS.ByteString)
 bacRead (R f) bs = f bs
 
+success :: a -> Reader a
+success a = R (\input -> (Just a, input))
+
+failure :: Reader a
+failure = R (\input -> (Nothing, input))
+
+byte :: Reader Word8
+byte = R (\input -> if BS.null input then (Nothing, BS.empty)
+                    else (Just $ BS.head input, BS.tail input))
+
+bindReader :: Reader a -> (a -> Reader b) -> Reader b
+bindReader r f = R (\input -> case bacRead r input of
+                                (Nothing, output) -> (Nothing, output)
+                                (Just val, output) -> bacRead (f val) output)
+
+instance Monad Reader where
+  return = success
+  (>>=) = bindReader
+
 nullAP :: Reader ()
-nullAP = R (\input -> if (BS.head input) == 0x00 then (Just (), BS.tail input)
-                      else (Nothing, BS.tail input))
+nullAP = do
+  b <- byte
+  if b == 0x00 then success () else failure
